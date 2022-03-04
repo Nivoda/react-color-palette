2c5540d559a79453467a56327625e26875e5af07
adebayo-maborukoje <adebayo.maborukoje2004@gmail.com>
2022-03-04 17:29:20 +0100

    with npm

--- src/utils/validate.util.ts
+++ src/utils/validate.util.ts
@@ -1,7 +1,7 @@
 const is_hex = /^#?(?:[0-9a-f]{3}){1,2}$/i;
 
 export function validHex(value: string): boolean {
-  console.log("it");
+  console.log("it", value);
 
   return is_hex.test(value);
 }
