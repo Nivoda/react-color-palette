06166337a9eaaf9cf393c94fb373d387132237e4
adebayo-maborukoje <adebayo.maborukoje2004@gmail.com>
2022-03-04 16:31:15 +0100

    fix validator

--- src/utils/validate.util.ts
+++ src/utils/validate.util.ts
@@ -1,11 +1,5 @@
-const hex = /[0-9A-F]/i;
+const is_hex = /^#?(?:[0-9a-f]{3}){1,2}$/i;
 
 export function validHex(value: string): boolean {
-  if (value.startsWith("#")) {
-    value = value.slice(1);
-
-    return hex.test(value[value.length - 1]);
-  }
-
-  return (/\w/i.test(value) && !/\d/.test(value)) || value === "";
+  return is_hex.test(value);
 }
